d4d59a7178666a868dafa2e91d1425a81823ac11
ankusht <ankushtiwari291@gmail.com>
2017-06-05 21:54:47 +0530

    Add files via upload

---
+++ setting_ships.py
@@ -0,0 +1,77 @@
+from random import randint
+from sys import argv
+script,file = argv
+matrix = [['0' for x in range(10)]for y in range(10)]
+ship =""
+ships = ['C','B','R','S','D']
+length = [5,4,3,3,2]
+txt = open(file,'w')		
+
+#place the ship horizontally
+def write_at_x(a,b,x,l):
+	for i in range(b,b+l):
+		matrix[a][i]=x
+		
+#place the ship vertically
+def write_at_y(a,b,x,l):
+	for i in range(a,a+l):
+		matrix[i][b] = x
+
+#check if the ship can be placed at the random coordinates
+def check(a,b,d,l):
+	if matrix[a][b]=='C' or matrix[a][b]=='B' or matrix[a][b]=='R' or matrix[a][b]=='S' or matrix[a][b]=='D':
+		return False
+	flag=0
+	if d==0:
+		if b+l>10:
+			return False
+		for i in range(b+1,b+l):
+			if matrix[a][i]=='C' or matrix[a][i]=='B' or matrix[a][i]=='R' or matrix[a][i]=='S' or matrix[a][i]=='D':
+				flag=1
+				break
+		if flag==1:
+			return False
+		else:
+			return True
+	else:
+		if a+l>10:
+			return False
+		for i in range(a+1,a+l):
+			if matrix[i][b]=='C' or matrix[i][b]=='B' or matrix[i][b]=='R' or matrix[i][b]=='S' or matrix[i][b]=='D':
+				flag=1
+				break
+		if flag==1:
+			return False
+		else:
+			return True
+										
+
+#place all the ships
+idx=0
+for x in ships:
+	while(True):
+		a = randint(0,9)
+		b = randint(0,9)
+		direction = randint(0,1)
+		if(direction==0):
+			if not(check(a,b,direction,length[idx])):
+				continue
+			else:
+				write_at_x(a,b,x,length[idx])
+				break
+		else:
+			if not(check(a,b,direction,length[idx])):
+				continue
+			else:
+				write_at_y(a,b,x,length[idx])
+				break			
+	idx=idx+1			
+
+for i in range(10):
+	for j in range(10):
+		if j!=9:
+			ship+=matrix[i][j]+" "
+		else:
+			ship+=matrix[i][j]+"\n"			
+
+txt.write(ship)				
